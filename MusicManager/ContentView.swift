6651551cb570c6162e5ae31b9c8dc500778b622f
EduAlexxis <eduardoflamenco751@gmail.com>
2026-01-06 11:04:18 -0500

    Initial Commit

---
+++ MusicManager/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MusicManager
+//
+//  Created by Eduardo Flamenco on 1/6/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MusicManager/MusicManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicManagerApp.swift
+//  MusicManager
+//
+//  Created by Eduardo Flamenco on 1/6/26.
+//
+
+import SwiftUI
+
+@main
+struct MusicManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
